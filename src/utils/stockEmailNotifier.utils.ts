4fce2dafa920ed2ebb32c4d9ae75f53520eef9e4
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-09-27 02:22:24 +0200

    implement active subscriber notification and stock check

--- src/utils/stockEmailNotifier.utils.ts
+++ src/utils/stockEmailNotifier.utils.ts
@@ -1,5 +1,19 @@
 import Product from "../models/Product.model";
 import { sendEmail } from "../emails/email";
+import EmailSubscription from "../models/EmailSubscription.model";
+
+export async function getActiveSubscribers() {
+  try {
+    const subscribers = await EmailSubscription.findAll({
+      where: {
+        isActivated: true,
+      },
+    });
+    return subscribers;
+  } catch (error) {
+    throw error;
+  }
+}
 
 const previousStockState: Record<string, number> = {};
 
@@ -11,10 +25,18 @@ export async function checkStockAndSendEmail() {
     const quantity = product.quantity;
 
     if (quantity === 0 && !previousStockState[productId]) {
+      console.log("No stock");
       previousStockState[productId] = 0;
     } else if (quantity >= 5 && previousStockState[productId] === 0) {
-      const recipientEmail = "onboarding@resend.dev";
-      sendEmail(recipientEmail, product.name);
+      const subscribers = await getActiveSubscribers();
+      const recipientEmails = subscribers.map((subscriber) => subscriber.email);
+      for (const subscriberEmail of recipientEmails) {
+        sendEmail(subscriberEmail, product.name);
+        await EmailSubscription.update(
+          { isActivated: false },
+          { where: { email: subscriberEmail } }
+        );
+      }
       previousStockState[productId] = quantity;
     } else {
       previousStockState[productId] = quantity;
