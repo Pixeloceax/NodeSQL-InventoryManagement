3707aae74ab888e9344273cd279e1194d208b51a
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-11-01 03:04:53 +0100

    refact

--- src/controllers/quantityController.ts
+++ src/controllers/quantityController.ts
@@ -1,9 +1,22 @@
+import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
-export async function updateQuantity(id: string, quantity: number) {
-  const product = await Product.findByPk(id);
-  if (product) {
-    product.quantity = quantity;
-    await product.save();
+export async function updateQuantity(req: Request, res: Response) {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    if (product) {
+      product.quantity = parseInt(req.params.quantity);
+      await product.save();
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        message: "An error occurred while updating the product quantity",
+      });
   }
 }
