40d0e76e71e858d34d78e7c41870f97f4524f5cd
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-11-02 02:24:18 +0100

    use env V

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,10 +1,20 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("inventory_db", "axel", "axel", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
+import dotenv from "dotenv";
+dotenv.config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+export const sequelize = new Sequelize(
+  DB_NAME as string,
+  DB_USER as string,
+  DB_PASSWORD as string,
+  {
+    host: DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 export async function dbConnect() {
   sequelize
