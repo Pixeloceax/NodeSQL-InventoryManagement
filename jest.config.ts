a4817daab0426f5c692408ad513976e3cc6b9592
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-11-02 02:58:11 +0100

    jest config TS

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,10 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  verbose: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
 
 export default config;
